5626506e350a16bd5c5ba36fc28ff674b4c9b233
ViLaRa <vianey.lara.rangel@gmail.com>
2019-12-22 02:55:56 -0600

    fix(fetchAPI): return array with spells of my hook  useFetch

--- src/Data/SpellsData.js
+++ src/Data/SpellsData.js
@@ -1,54 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { BASE_URL } from '../Utils/constants';
 
+//My custom hook
 const useFetch = (url, defaultResponse) => {
+    // Declare a new state variable to save data from API
     const [data, setData] = useState(defaultResponse);
 
-    async function getDataFromAPI(url) {
-        try {
-            const response = await fetch(url, {key:key});
-            const data = await response.json();
-            setData({
-                isLoading: false,
-                data
+    //Function to ajax request
+    const key = '$2a$10$4tEN6D3D/FFAywchrprgNuppzyUXyGNtrfX3iiitvkT42GWuJhm0S';
+    const apiEndpoint = new URL('https://www.potterapi.com/v1/spells/'), params = { key: key };
+
+    const getDataOfSpells = (key, url) => {
+
+        Object.keys(params).forEach(key => apiEndpoint.searchParams.append(key, params[key]))
+
+        return fetch(apiEndpoint, { key: key })
+            .then((response) => response.json())
+            .then((info) => {
+                let spells = info;
+                return spells;
             })
-        } catch (e) {
-            console.error(e);
-        }
     }
-    
-    useEffect(() => {
-        getDataFromAPI(url);
-    }, [url]);
-    
-    return data;
+    return getDataOfSpells(key, apiEndpoint);
 }
 
-const key = '$2a$10$4tEN6D3D/FFAywchrprgNuppzyUXyGNtrfX3iiitvkT42GWuJhm0S';
-
-const urlSpells = new URL('https://www.potterapi.com/v1/spells'),
-params = {key: key}
-
-Object.keys(params).forEach(key => urlSpells.searchParams.append(key, params[key]))
-
-console.log(urlSpells);
-
 const Spells = () => {
-    // const apiEndpoint = (`${BASE_URL}`);
-    const userFetchResponse = useFetch(urlSpells, {isLoading:true, data: null});
-    
-    if (!userFetchResponse.data || userFetchResponse.isLoading) {
-        return console.log('Loading...');
-    } else {
-        console.log('erroooor');
-    }
+    // console.log(useFetch());
+    // const userFetchResponse = useFetch(apiEndpoint);
 
-    const spell  = userFetchResponse.data.data;
-    
-    console.log(spell);
     return (
-    <ul>Name: {spell}</ul>
-    );
+        <div>
+            <p>Hear Data from API</p>
+            {/* <p>{spell}</p> */}
+        </div>
+    )
 }
 
 export default Spells;

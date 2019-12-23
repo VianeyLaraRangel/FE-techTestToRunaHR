a22685db379b8031d633cc277b061517cf91ddb4
ViLaRa <vianey.lara.rangel@gmail.com>
2019-12-23 10:04:55 -0600

    Updates

---
+++ precache-manifest.953681a9522c63c515eec3bd66a3ba6a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0a621b45cbcf67bc9a8f0d7ec759e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c04d960724bb10bf6fa4",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6d46a1093e1c26507dbf",
+    "url": "./static/css/main.a0f69134.chunk.css"
+  },
+  {
+    "revision": "c04d960724bb10bf6fa4",
+    "url": "./static/js/2.5d361e22.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "./static/js/2.5d361e22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d46a1093e1c26507dbf",
+    "url": "./static/js/main.cd1d9f9f.chunk.js"
+  },
+  {
+    "revision": "07b8ac5ea5ca1ff3f8a4",
+    "url": "./static/js/runtime-main.61e705e5.js"
+  },
+  {
+    "revision": "7baf4d29e28b33e6d8ed450397d32a51",
+    "url": "./static/media/charmImg.7baf4d29.png"
+  },
+  {
+    "revision": "5ccf1232cb5524a40ce7700c70348484",
+    "url": "./static/media/curseImg.5ccf1232.png"
+  },
+  {
+    "revision": "bc0221d630e48281255eda208df2de8e",
+    "url": "./static/media/enchantmentImg.bc0221d6.png"
+  },
+  {
+    "revision": "fc562901c70c3018eddd633a84eeabdd",
+    "url": "./static/media/spellImg.fc562901.png"
+  }
+]);

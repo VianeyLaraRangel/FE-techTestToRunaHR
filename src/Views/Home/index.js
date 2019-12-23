90dd509f07a15b1411be517996cb54376b3ffdc4
ViLaRa <vianey.lara.rangel@gmail.com>
2019-12-23 11:18:04 -0600

    feat(Navbar): Adding componente with logo

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Spells from '../../Data/apiData';
+import Navbar from '../../Components/Navbar/Navbar';
 
 import './style.css';
 
 const Home = () => (
     <div>
+        <nav>
+            <Navbar />
+        </nav>
         <Spells />
     </div>
 );
